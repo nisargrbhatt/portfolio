944325e519b7cefdcc7982c8c941e69964feab47
nisargrbhatt <nisargrbhatt@gmail.com>
2022-06-18 22:31:37 +0530

    Added Google ESLint Style guide

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
+/**
+ *  @param {AppProps} props
+ *  @return {any}
+ */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
