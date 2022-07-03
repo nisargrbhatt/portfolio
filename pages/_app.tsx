6f0a9dc4b1f55d9d6a9bc749ff2c605ec64d4a18
nisargrbhatt <nisargrbhatt@gmail.com>
2022-07-03 19:26:35 +0530

    Added MUI & Auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,38 @@
-import '../styles/globals.css';
+import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { UserProvider } from '@auth0/nextjs-auth0';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+
+import createEmotionCache from '../utility/createEmotionCache';
+import lightTheme from '@styles/theme/lightTheme';
+
+const clientSideEmotionCache = createEmotionCache();
 
 /**
  *  @param {AppProps} props
  *  @return {JSX.Element}
  */
 function MyApp({ Component, pageProps }: AppProps) {
+  const { emotionCache = clientSideEmotionCache } = pageProps;
+
   // eslint-disable-next-line react/jsx-props-no-spreading
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <UserProvider user={pageProps.user}>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+          </Head>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </UserProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
