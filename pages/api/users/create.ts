8d007cec99db3b480bf804637b12a03370212379
nisargrbhatt <nisargrbhatt@gmail.com>
2022-07-16 22:17:42 +0530

    Prisma added

---
+++ pages/api/users/create.ts
@@ -0,0 +1,25 @@
+import prisma from '@lib/prisma';
+import { User } from '@prisma/client';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+
+/**
+ * Add User
+ * @date 16/7/2022 - 8:28:18 pm
+ * @author Nisarg Bhatt
+ * @async
+ * @param {NextApiRequest} req
+ * @param {NextApiResponse} res
+ */
+const handler: NextApiHandler<User> = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const data = req.body;
+  const user = await prisma.user.create({
+    data,
+  });
+
+  res.json(user);
+};
+
+export default handler;
---
+++ pages/api/users/list.ts
@@ -0,0 +1,31 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { User } from '@prisma/client';
+import prisma from '@lib/prisma';
+
+/**
+ * Get List of Users
+ * @date 16/7/2022 - 8:16:28 pm
+ *
+ * @param {NextApiRequest} req
+ * @param {NextApiResponse} res
+ */
+const handler: NextApiHandler<User[]> = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const page = parseInt(
+    req.query?.page?.at(0) ?? (req.query?.page as string) ?? '1'
+  );
+  const limit = parseInt(
+    req.query?.limit?.at(0) ?? (req.query?.limit as string) ?? '10'
+  );
+
+  const users = await prisma.user.findMany({
+    skip: page,
+    take: limit,
+  });
+
+  res.json(users);
+};
+
+export default handler;
