dbfe1da7760ea901c56d9af09827e64097893ea9
Nisarg Bhatt <nisargrbhatt@gmail.com>
2025-03-02 14:35:14 +0530

    Base Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: "https://nisargbhatt.com",
+  output: "static",
+  adapter: cloudflare(),
+  integrations: [sitemap(), mdx(), react()],
+});
