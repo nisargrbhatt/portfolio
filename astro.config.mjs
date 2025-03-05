5da21a8434c28aae593aef0e1d8d41d2bfe0fbc1
Nisarg Bhatt <nisargrbhatt@gmail.com>
2025-03-06 00:34:53 +0530

    fix: cloudflare plugin removed as no SSR usage, animate removed and shadcn button added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-// import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
@@ -21,8 +20,5 @@ export default defineConfig({
   },
   site: "https://nisargbhatt.com",
   output: "static",
-  // adapter: cloudflare({
-  //   imageService: "passthrough"
-  // }),
   integrations: [sitemap(), mdx(), react()],
 });
