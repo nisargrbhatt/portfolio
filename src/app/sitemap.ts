52b7297a914e53dfc9f3848eb86f0fda3202d806
Nisarg Bhatt <nisargrbhatt@gmail.com>
2025-10-14 22:08:03 +0530

    Sitemap fix added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { SITE_URL } from "@/lib/common";
 import type { MetadataRoute } from "next";
+import { projects } from "./project/projects";
+import { blogs } from "./blog/blogs";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
@@ -15,17 +17,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.5,
     },
-    {
-      url: new URL("/blog/react-query-fetch", SITE_URL).toString(),
-      lastModified: new Date(),
-      changeFrequency: "weekly",
+    ...Object.values(blogs).map((b) => ({
+      url: new URL(`/blog/${b.slug}`, SITE_URL).toString(),
+      lastModified: new Date(b.createdAt),
+      changeFrequency: "weekly" as const,
       priority: 0.8,
-    },
+    })),
     {
       url: new URL("/project", SITE_URL).toString(),
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
     },
+    ...projects.map((p) => ({
+      url: new URL(`/project/${p.id}`, SITE_URL).toString(),
+      lastModified: new Date(),
+      changeFrequency: "monthly" as const,
+      priority: 0.5,
+    })),
   ];
 }
